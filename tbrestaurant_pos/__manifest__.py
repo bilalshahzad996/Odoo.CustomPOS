dd84959f4a96cad9c80b1c752667e0d64388c165
Bilal Shahzad <bilalshahzad199@gmail.com>
2026-06-25 13:57:26 +0500

    Add files via upload

---
+++ tbrestaurant_pos/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Restaurant POS Kitchen Receipt",
+    "version": "19.0.1.0.0",
+    "author": "Bilal Shahzad",
+    "category": "Point of Sale",
+    "depends": ["point_of_sale", "pos_restaurant", "pos_hr"],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "restaurant_pos/static/src/xml/kitchen_receipt.xml",
+            "restaurant_pos/static/src/xml/kitchen_change_receipt.xml",
+            "restaurant_pos/static/src/xml/control_buttons.xml",
+            "restaurant_pos/static/src/js/kitchen_receipt_print.js",
+            "restaurant_pos/static/src/js/kitchen_send_patch.js",
+            "restaurant_pos/static/src/js/control_buttons_patch.js",
+            "restaurant_pos/static/src/js/order_delete_guard.js",
+            "restaurant_pos/static/src/js/order_line_delete_guard.js"
+        ]
+    },
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3"
+}
